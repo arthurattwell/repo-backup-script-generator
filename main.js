0a0ea9f50a4c2846ee1142d6c90f17d3dba48a60
Arthur Attwell <arthur@arthurattwell.com>
2018-08-22 10:22:19 +0200

    Don't try clone if folder exists
    Also remove ref to batch file, since myrepos is only for unix anyway.

--- main.js
+++ main.js
@@ -19,12 +19,14 @@ function listRepos(data) {
     });
 }
 
-// Generate the shell script/batch file text
+// Generate the shell script text
 function generateScript(data, entity, outputContainer) {
-    outputContainer.innerHTML += "mkdir " + data[0].owner.login + "<br>";
+    // Make the entity directory if it doesn't exist.
+    outputContainer.innerHTML += "mkdir -p " + data[0].owner.login + "<br>";
     data.forEach(function(data) {
         outputContainer.innerHTML += "cd " + data.owner.login;
         outputContainer.innerHTML += " && git clone git@github.com:" + data.full_name + ".git";
+        outputContainer.innerHTML += " || echo 'Folder exists, not cloning " + data.name + "'";
         outputContainer.innerHTML += " && cd " + data.name;
         outputContainer.innerHTML += " && mr register";
         outputContainer.innerHTML += " && cd ../../";
